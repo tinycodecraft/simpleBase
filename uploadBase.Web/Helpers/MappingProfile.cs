346bb0051301acbecda7778b3599a69a19782dcc
tinycodecraft <raymond.cw.yau@gmail.com>
2025-02-27 16:20:26 +0800

    add comment to describe web service collection setup

---
+++ uploadBase.Web/Helpers/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace uploadBase.Web.Helpers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //CreateMap<CreditCard, CreditCardModel>();
+            //CreateMap<CreditCardModel, CreditCard>();
+            // Add other mappings as needed
+
+            //CreateMap<UserIncome, UserIncomeModel>().ReverseMap();
+            //CreateMap<UserBudget, UserBudgetModel>().ReverseMap();
+            //CreateMap<FamilyMemberRequest, FamilyMemberRequestModel>().ReverseMap();
+        }
+    }
+}
--- uploadBase.Web/Program.cs
+++ uploadBase.Web/Program.cs
@@ -5,7 +5,13 @@ using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Razor;
 using System.Text.Json.Serialization;
+using uploadBase.Shared.Tools;
+using uploadBase.Shared;
 using uploadBase.Web.Helpers;
+using static uploadBase.Shared.Interfaces;
+
+/*Bootstrap logger
+ */
 
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
@@ -15,6 +21,10 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
     //WebRootPath = "wwwroot",
 });
 
+/*configure appsetting options
+ */
+
+builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.Configure<FormOptions>(opt =>
 {
@@ -31,26 +41,94 @@ builder.Services.Configure<IISServerOptions>(opt =>
 
 });
 
-//Setup DB Context
+
+/*UseSerilog configuration
+ */
+
+/*Setup DB Context
+ */
 //builder.Services.AddDbContext<BFAContext>();
 
 
 builder.Services.AddTransient<ProblemDetailsFactory, CustomProblemDetailsFactory>();
 
+/*inject service
+ */
 
-// Add services to the container.
+/*setup cors policy
+ */
+
+
+/*setup signalr
+ */
+//builder.Services.AddSignalR(hubOptions =>
+//{
+//    hubOptions.KeepAliveInterval = TimeSpan.FromSeconds(15);
+//    hubOptions.HandshakeTimeout = TimeSpan.FromSeconds(15);
+//    hubOptions.EnableDetailedErrors = true;
+//});
+
+/*setup controller
+ */
 builder.Services.AddCustomLocalization("en-US", "zh-HK");
 builder.Services.AddControllersWithViews()
     .AddJsonOptions(opt => opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles)
     .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
     .AddDataAnnotationsLocalization();
 
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+/*add compression
+ */
+
 builder.Services.AddSwaggerGen(option =>
 {
 
 });
+
+/*add authentication
+ */
+
+/*Try to add session
+ */
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromDays(1);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+    options.Cookie.Name = ".uploadBase.Session";
+});
+
+/*add hub gateway (observable for creating subscriber through hub responding to signalr client's request=subscribing method)
+ */
+//builder.Services.AddSingleton(typeof(IResultGateway<>), typeof(ModelResultGateway<>));
+
+
+/*add mediatr request routing
+ */
+//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Interfaces>());
+
+//builder.Services.AddHsts(options =>
+//{
+//    options.Preload = true;
+//    options.IncludeSubDomains = true;
+//    options.MaxAge = TimeSpan.FromDays(60);
+//    options.ExcludedHosts.Add("example.com");
+//    options.ExcludedHosts.Add("www.example.com");
+//});
+
+//builder.Services.AddHttpsRedirection(options =>
+//{
+//    options.RedirectStatusCode =307; //temporaryredirect
+//    options.HttpsPort = 5001;
+//});
+
 
 var app = builder.Build();
 
